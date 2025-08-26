349f122b6052123ffaff1177faac3de04b8a656a
Maria Ines Parnisari <maineparnisari@gmail.com>
2025-08-26 11:59:40 -0700

    fix ssl error

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,60 +5,55 @@ import com.authzed.api.materialize.v0.WatchPermissionsServiceGrpc;
 import com.authzed.api.v1.*;
 import com.authzed.grpcutil.BearerToken;
 import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
 import javax.net.ssl.*;
-import java.security.cert.X509Certificate;
+import java.security.KeyStore;
 
 
 public class Main {
-    /**
-     * Configures SSL to trust all certificates and bypass hostname verification.
-     * This allows the application to connect to servers with untrusted or self-signed certificates.
-     * WARNING: This approach is insecure for production use.
-     */
-    private static void configureTrustAllSSL() throws Exception {
-        TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    @Override
-                    public X509Certificate[] getAcceptedIssuers() {
-                        return null;
-                    }
-                    @Override
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                        // Do nothing - trust all clients
-                    }
-                    @Override
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                        // Do nothing - trust all servers
-                    }
-                }
-        };
-
-        SSLContext sc = SSLContext.getInstance("SSL");
-        sc.init(null, trustAllCerts, new java.security.SecureRandom());
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
-    }
-
     public static void main(String[] args) throws Exception {
-        configureTrustAllSSL();
-        
-        ManagedChannel channel = ManagedChannelBuilder
-                .forTarget("URL_HERE")
-                .useTransportSecurity()
+        KeyStore systemKeyStore = KeyStore.getInstance("KeychainStore");
+        systemKeyStore.load(null, null);
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(systemKeyStore);
+        SslContext sslContext = GrpcSslContexts.forClient()
+                .trustManager(tmf)
                 .build();
 
-        BearerToken bearerToken = new BearerToken("TOKEN_HERE");
-        WatchPermissionsServiceGrpc.WatchPermissionsServiceBlockingStub permissionsService = WatchPermissionsServiceGrpc
-                .newBlockingStub(channel)
+        ManagedChannel channel = NettyChannelBuilder
+                .forTarget("url-here")
+                .sslContext(sslContext)
+                .build();
+
+        BearerToken bearerToken = new BearerToken("token-here");
+        WatchPermissionsServiceGrpc.WatchPermissionsServiceStub watchClient = WatchPermissionsServiceGrpc
+                .newStub(channel)
                 .withCallCredentials(bearerToken);
+
         try {
             WatchPermissionsRequest request = WatchPermissionsRequest.newBuilder().build();
 
-            permissionsService.watchPermissions(request).forEachRemaining(response -> {
-                System.out.println("Received permission update: " + response);
+            watchClient.watchPermissions(request, new io.grpc.stub.StreamObserver<>() {
+                @Override
+                public void onNext(com.authzed.api.materialize.v0.WatchPermissionsResponse response) {
+                    System.out.println("Received permission update: " + response);
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    System.out.println("Stream error: " + t.getMessage());
+                }
+
+                @Override
+                public void onCompleted() {
+                    System.out.println("Stream completed");
+                }
             });
+            
+            // Keep the main thread alive to receive streaming responses
+            Thread.sleep(30000);
         } catch (Exception e) {
             System.out.println("Failed to call watch: " + e.getMessage());
         }
